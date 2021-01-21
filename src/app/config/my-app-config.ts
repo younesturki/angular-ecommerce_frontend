a43522e1de74a5f85361920fedab70406469aaf7
younes.turki <younesturki@live.fr>
2021-01-21 16:45:22 +0100

    Okta/Login/Logout

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa43kthsDpAg0eGQ5d6',
+        issuer: 'https://dev-78963574.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile', 'email']
+    }
+
+}
